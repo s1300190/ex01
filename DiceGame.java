c0285800cdbe18345e599f1d701ed6c7e0b80723
s1300190 <s1300190@u-aizu.ac.jp>
2023-06-15 17:20:20 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Random;
 class DiceGame{
     public static void main(String[] args){
@@ -5,6 +6,12 @@ class DiceGame{
         int die1 = dice.nextInt(6)+1;
         int die2 = dice.nextInt(6)+1;
         int total = die1 + die2;
+        
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println("Hello, " + str + "!");
+
         System.out.println("Rolling dice...");
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
